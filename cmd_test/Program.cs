e27c7884aa7550dfcbb0e5605d30fb5b4b6931e4
Deng Yu <jardy@vip.qq.com>
2020-03-15 11:54:35 +0800

    貌似又可以了

--- cmd_test/Program.cs
+++ cmd_test/Program.cs
@@ -14,7 +14,7 @@ namespace cmd_test
         static void Main(string[] args)
         {
             //测试VS2019中的 GitHub连接
-            Console.WriteLine("貌似连接不上了...");
+            Console.WriteLine("可恶");
 
             //MD5加解密
             /*MD5 md5 = MD5.Create();
